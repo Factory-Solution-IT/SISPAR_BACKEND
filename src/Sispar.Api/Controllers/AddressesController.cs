39e7b6ddb3e735e5cfe1fbf7fb72d01774b9eab1
Felipe Santos <flpsno@hotmail.com>
2020-06-03 23:33:20 -0300

    adicionado busca cep versao 1

---
+++ src/Sispar.Api/Controllers/AddressesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sispar.Infra.ExternalServices;
+
+namespace Sispar.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+
+        [HttpGet("{zipCode}")]
+        public async Task<IActionResult> GetByZipCoode(string zipCode)
+        {
+            var addressService = new AddressService(zipCode);
+
+            var result = await addressService.DoSearh();
+            return Ok(result);
+        }
+    }
+}
